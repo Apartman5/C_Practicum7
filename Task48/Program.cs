b42d88afdcb1c726b80d7e0dd6b225b58f19bf3a
Igor Kazakov <kiw@lefterova.ru>
2023-03-26 15:59:48 +0500

    first commit

---
+++ Task48/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] CreateOutputArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int m = Prompt("Input string");
+int n = Prompt("Input columns");
+CreateOutputArray(m, n);
---
+++ Task49/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] CreateOutputArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int[,] CreateNewArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            matrix[i, j] = matrix [i,j] * matrix [i,j];
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+int m = Prompt("Input string");
+int n = Prompt("Input columns");
+CreateNewArray(CreateOutputArray(m, n));
